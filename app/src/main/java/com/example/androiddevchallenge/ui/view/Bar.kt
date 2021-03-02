e1b1c8001d503e589a51f927072cb6c512d105dc
gongzhenxing <574443457@qq.com>
2021-03-03 00:52:23 +0800

    Fix

--- app/src/main/java/com/example/androiddevchallenge/ui/view/Bar.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/view/Bar.kt
@@ -2,7 +2,11 @@ package com.example.androiddevchallenge.ui.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
